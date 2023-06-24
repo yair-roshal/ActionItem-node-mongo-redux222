d67306695656f2f28d708665ed1fe0c82ce101b9
yair-roshal <yair7705778@gmail.com>
2023-06-25 01:02:46 +0300

    fix

--- client/src/components/AllProfilesScreen.js
+++ client/src/components/AllProfilesScreen.js
@@ -1,32 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+} from '@mui/material'
 
 const HistoryScreen = () => {
   const navigate = useNavigate()
   const profiles = useSelector(state => state.profiles)
+  const [filterName, setFilterName] = useState('')
+  const [filterCountry, setFilterCountry] = useState('')
 
   const handleProfileClick = id => {
     navigate(`/profile/${id}`)
   }
 
+  const filteredProfiles = profiles.filter(profile => {
+    const fullName = `${profile.name.title} ${profile.name.first} ${profile.name.last}`
+    return (
+      fullName.toLowerCase().includes(filterName.toLowerCase()) &&
+      profile.location.country
+        .toLowerCase()
+        .includes(filterCountry.toLowerCase())
+    )
+  })
+
   return (
     <div>
       <h1>All Profiles Screen</h1>
 
-      {profiles &&
-        profiles.map(profile => (
-          <div
-            key={profile.cell}
-            onClick={() => handleProfileClick(profile.id)}
-          >
-            <h3>
-              {profile.name.title} {profile.name.first} {profile.name.last}{' '}
-            </h3>
+      <div>
+        <TextField
+          label='Filter by name'
+          value={filterName}
+          onChange={e => setFilterName(e.target.value)}
+          variant='outlined'
+          size='small'
+          style={{ marginRight: 10 }}
+        />
+        <TextField
+          label='Filter by country'
+          value={filterCountry}
+          onChange={e => setFilterCountry(e.target.value)}
+          variant='outlined'
+          size='small'
+        />
+      </div>
 
-            <p>{profile.email}</p>
-          </div>
-        ))}
+      {filteredProfiles.length > 0 ? (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Thumbnail</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Country</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredProfiles.map(profile => (
+                <TableRow
+                  key={profile.cell}
+                  onClick={() => handleProfileClick(profile.id)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <TableCell>
+                    <img
+                      src={profile.picture.thumbnail}
+                      alt='Profile Thumbnail'
+                      style={{ width: 50, height: 50 }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    {profile.name.title} {profile.name.first}{' '}
+                    {profile.name.last}
+                  </TableCell>
+                  <TableCell>{profile.gender}</TableCell>
+                  <TableCell>{profile.location.country}</TableCell>
+                  <TableCell>{profile.phone}</TableCell>
+                  <TableCell>{profile.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <p>No profiles found.</p>
+      )}
     </div>
   )
 }
